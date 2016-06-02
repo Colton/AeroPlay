cf5464a772359c6a5b9a32f2b726a1481797204c
Colton Anglin <colton01@icloud.com>
2016-06-01 21:17:21 -0500

    Initial Commit

---
+++ AirPlay3/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AirPlay3
+//
+//  Created by Colton Anglin on 6/1/16.
+//  Copyright © 2016 Colton Anglin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AirPlay3/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AirPlay3
+//
+//  Created by Colton Anglin on 6/1/16.
+//  Copyright © 2016 Colton Anglin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
