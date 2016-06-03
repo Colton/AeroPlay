80b66340b54ebc30ac306a562b8ca7954d06c2fa
Colton Anglin <colton01@icloud.com>
2016-06-03 17:37:20 -0500

    Removed for-loop test

--- AeroPlay/AppDelegate.swift
+++ AeroPlay/AppDelegate.swift
@@ -15,12 +15,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
         
-        for i in 0...200 {
-            let server = AirplayServer(serverName: "Airplay Server", UID: UInt8(i), serverPortNumber: 6050 + (i * 10), controlPortNumber: 6051 + (i * 10), timingPortNumber: 6052 + (i * 10))
+            let server = AirplayServer(serverName: "Airplay Server", UID: 1, serverPortNumber: 6050, controlPortNumber: 6051, timingPortNumber: 6052)
             server.start() // Start Airplay server
-
-        }
-        
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
